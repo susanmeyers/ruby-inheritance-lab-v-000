98ef271eb874321a8c735f6857ed4451115803ba
Susan Meyers <smmeyers24@gmail.com>
2018-10-15 04:23:37 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,7 +3,20 @@ class Teacher < User
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
   
   def teach
-    sample(random: rng)
+    
+30
+down vote
+accepted
+use Array#sample:
+
+@countries = ["Canada", "Denmark", "Germany", "Isle of Man", "Namibia", "Qatar", "South Africa",  "United Kingdom","United States"]
+
+random_country = @countries.sample
+# => "Canada"
+
+random_country = @countries.sample
+# => "United Kingdom"
+shareimprove this answer
     
   end
 
